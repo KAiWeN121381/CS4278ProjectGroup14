c592b89dee7d021e0e58e555de8a97eb4123d348
Funniguy <liao.li@vanderbilt.edu>
2023-09-26 15:47:20 -0500

    Fix path unknown bug for NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function NavBar () {
     return (
@@ -14,10 +14,11 @@ export default function NavBar () {
 }
 
 function CustomLink ({to, children, ...props}) {
-    const path = window.location.pathname
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path : resolvedPath.pathname, end : true})
 
     return (
-        <li className={path === to ? "active" : ""}>
+        <li className={isActive ? "active" : ""}>
             <Link to={to} {...props}>
                 {children}
             </Link>
