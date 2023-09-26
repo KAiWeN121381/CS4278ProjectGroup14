270c8a248f93055c666c14650f2579e46cf2e249
Funniguy <liao.li@vanderbilt.edu>
2023-09-26 10:36:38 -0500

    Update post format and layouts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,31 @@
+import { ButtonBase, Grid } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import * as React from 'react';
+import { blue } from "@mui/material/colors";
+
+const Img = styled('img')({
+    margin: 'auto',
+    display: 'block',
+    maxWidth: '100%',
+    maxHeight: '100%',
+});
+
 export default function Post(props) {
     return (
-        <div className="rectangle">
-            <h1>{props.title}</h1>
-            <img 
-                src="https://legacy.reactjs.org/logo-og.png" 
-                alt="Temp photo" 
-                height={100}
-                weight={100}
-                />
-            <p>{props.text}</p>
-        </div>
+        <Paper>
+           <Grid container spacing={1}>
+                <Grid item>
+                    <h3>{props.title}</h3>
+                    <ButtonBase sx={{ width: 128, height: 128}}>
+                        <Img 
+                        src="https://legacy.reactjs.org/logo-og.png" 
+                        alt="Temp photo">
+                        </Img>
+                    </ButtonBase>
+                    <p>{props.text}</p> 
+                </Grid>
+            </Grid>
+        </Paper>
     )
 }
--- src/components/PostGroup.jsx
+++ src/components/PostGroup.jsx
@@ -1,33 +1,30 @@
 import { useState } from "react";
 import Post from "./Post";
+import * as React from 'react';;
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 
 export default function PostGroup() {
     let posts = [
         <Post title="Title 1" text="text 1"/>,
         <Post title="Title 2" text="text 2"/>,
         <Post title="Title 3" text="text 3"/>,
-        <Post title="Title 4" text="text 4"/>
+        <Post title="Title 4" text="text 4"/>,
+        <Post title="Title 5" text="text 5"/>,
+        <Post title="Title 6" text="text 6"/>
     ];
 
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return ( 
-    <>
+    <Box sx={{flexGrow: 1}}>
         {posts.length === 0 && <h1>No posts are found</h1>}
-        <ul className="list-group">
+        <Grid container spacing={0.5}>
             {posts.map((post, index) => (
-                <li 
-                    className= {
-                        selectedIndex === index
-                            ? "list-group-item active"
-                            : "list-group-item"
-                        } 
-                    key={index} // change to key={post.id} later when implemented
-                    onClick={() => {setSelectedIndex(index);}}
-                >
+                <Grid item xs={4} key={index}>
                     {post}
-                </li>
+                </Grid>
             ))}
-        </ul>
-    </>)
+        </Grid>
+    </Box>)
 }
