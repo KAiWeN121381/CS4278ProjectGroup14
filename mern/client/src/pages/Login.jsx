94c697c127cd67ff75312d94cdccd46b78cea9d7
Funniguy <liao.li@vanderbilt.edu>
2023-11-13 16:39:33 -0600

    return to previous page after login

--- mern/client/src/pages/Login.jsx
+++ mern/client/src/pages/Login.jsx
@@ -1,14 +1,19 @@
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
+    const navigate = useNavigate();
+
     return (
         <GoogleOAuthProvider clientId='968768895156-u0n5f8i1hh1hpg97kn0k49loulgrr4mk.apps.googleusercontent.com'>
             <GoogleLogin
             onSuccess={credentialResponse => {
                 // Contains email, family_name, given_name, and other stuff that I dont think we need 
                 var decodedResponse = jwtDecode(credentialResponse.credential);
-                console.log(decodedResponse);
+                
+                console.log(decodedResponse.email);
+                navigate(-1); // Returns to the page it came from
             }}
             onError={() => {
                 console.log('Login Failed');
