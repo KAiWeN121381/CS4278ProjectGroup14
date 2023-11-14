6e0d547c546bd686803ae3a37db5bb2d96f2cff3
KAiWeN121381 <kaiwen.shi@vanderbilt.edu>
2023-11-13 18:37:35 -0600

    1

--- mern/server/server.mjs
+++ mern/server/server.mjs
@@ -6,18 +6,7 @@ import records from "./routes/record.mjs";
 const PORT = 5050;
 const app = express();
 
-const allowedOrigins = ["http://52.15.93.98"];
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
+app.use(cors({ origin: "*" }));
 
 app.use(express.json());
 
