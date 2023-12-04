baaefed7d1a950cdfd52798670dfe92221209a0c
Funniguy <liao.li@vanderbilt.edu>
2023-12-04 16:05:51 -0600

    fixing create profile not shown

--- mern/client/src/pages/Login.jsx
+++ mern/client/src/pages/Login.jsx
@@ -47,7 +47,7 @@ export default function LoginPage() {
                     navigate(-1);
                 }
                 else{
-                    navigate(`/newprofile`)
+                    navigate(`/createprofile`)
                 }
             }}
             onError={() => {
